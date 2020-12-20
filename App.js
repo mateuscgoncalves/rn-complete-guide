22a337c75cfde112fbe7d6cac0fc29e25d965e94
Mateus Gonçalves <mateus_c_g@hotmail.com>
2020-12-20 16:37:41 +0100

    add styles to a const style

--- App.js
+++ App.js
@@ -4,9 +4,9 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-      <View>
-        <View style={{padding: 50}}>
-          <TextInput placeholder={'Enter your next goal:'} style={{borderColor: 'black', borderWidth:1, padding: 10}}/>
+      <View style={styles.screen}>
+        <View style={styles.inputContainer}>
+          <TextInput placeholder={'Enter your next goal:'} style={}/>
           <Button title={'Add'}/>
         </View>
         <View>
@@ -17,4 +17,18 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  screen:{
+    padding: 50
+  },
+  inputContainer:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input:{
+    borderColor: 'black',
+    borderWidth:1,
+    padding: 10,
+    flexGrow: 3
+  }
 });
