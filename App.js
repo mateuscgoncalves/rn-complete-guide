44373d0ac84fe669cbf37df9e47bfec800eb63ab
Mateus Gonçalves <mateus_c_g@hotmail.com>
2020-12-20 17:59:57 +0100

    add some styles to list item

--- App.js
+++ App.js
@@ -1,16 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React,{useState} from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([])
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  }
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+    console.log(courseGoals);
+  }
+
   return (
       <View style={styles.screen}>
         <View style={styles.inputContainer}>
-          <TextInput placeholder={'Enter your next goal:'} style={}/>
-          <Button title={'Add'}/>
+          <TextInput placeholder={'Enter your next goal:'} style={styles.input} onChangeText={goalInputHandler} value={enteredGoal}/>
+          <Button title={'Add'} onPress={addGoalHandler}/>
         </View>
         <View>
-
+          {
+            courseGoals.map((goal) =>
+                <View style={styles.listItem} key={goal}>
+                  <Text>{goal}</Text>
+                </View>
+            )
+          }
         </View>
       </View>
   );
@@ -30,5 +47,12 @@ const styles = StyleSheet.create({
     borderWidth:1,
     padding: 10,
     flexGrow: 3
+  },
+  listItem:{
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
   }
 });
